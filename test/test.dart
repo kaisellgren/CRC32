6763bc937830c16520f84ecb26e537a66ea9d1ae
Devon Carew <devoncarew@gmail.com>
2014-09-19 14:59:16 -0700

    upgrade to the latest unittest version

--- test/test.dart
+++ test/test.dart
@@ -2,7 +2,6 @@ import 'package:unittest/unittest.dart';
 import 'package:crc32/crc32.dart';
 
 void main() {
-
   test('basic', () {
     expect(CRC32.compute("testing out".codeUnits), 316532775);
   });
